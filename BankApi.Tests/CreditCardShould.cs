d4fe3abe1859070d404a318d8ff0948869fd5342
Sergio Sanchez <sergiorodrigo.sanchezrios@sivantos.com>
2025-03-01 22:43:39 +0100

    build up validation for cvv

---
+++ BankApi.Tests/CreditCardShould.cs
@@ -0,0 +1,55 @@
+﻿using BankApi.Models;
+
+using FluentAssertions;
+
+namespace BankApi.Tests;
+
+public class CreditCardShould
+{
+  public static IEnumerable<object[]> GetCardNumbers() => 
+      Enumerable.Range(0,10).Select(n => (object[])[n]);
+
+  [Theory]
+  [MemberData(nameof(GetCardNumbers))]
+  public void ReturnValidCardNumber(uint expected)
+  {
+    var ch = char.Parse($"{expected}");
+    var result = CreditCardExtensions.CharToDigit(ch);
+
+    result.Match(
+        r => r.Should().Be(expected),
+        _ => throw new Exception("Should not happen"));
+  }
+
+    [Theory]
+    [InlineData('a')]
+    [InlineData('Z')]
+    public void ReturnInvalidCardNumber(char ch)
+    {
+        var result = CreditCardExtensions.CharToDigit(ch);
+        result.Match(
+            _ => throw new Exception("Should not happen"),
+            errs => errs.Should().Contain(e => e.Message.Equals($"Invalid character: {ch}")));
+    }
+
+    [Theory]
+    [InlineData("1234567890")]
+    [InlineData("0000000000")]
+    [InlineData("9999999999")]
+    public void ReturnValidCardNumbers(string value)
+    {
+        var result = CreditCardExtensions.ValidateAllDigits(value);
+        result.Match(
+            r => r.Should().NotBeEmpty(),
+            _ => throw new Exception("Should not happen"));
+    }
+
+    [Fact]
+    public void ReturnInvalidCardNumbers()
+    {
+        var result = CreditCardExtensions.ValidateAllDigits("xy123");
+        result.Match(
+            _ => throw new Exception("Should not happen"),
+            errs => errs.Should().Contain(e => e.Message.StartsWith("Invalid character")));
+    }
+}
--- BankApi/Models/CreditCard.cs
+++ BankApi/Models/CreditCard.cs
@@ -1,4 +1,6 @@
-﻿using LanguageExt;
+﻿using System.Reflection.Metadata.Ecma335;
+
+using LanguageExt;
 using LanguageExt.Common;
 
 namespace BankApi.Models;
@@ -17,10 +19,32 @@ public record CardNumber(Seq<int> Number);
 
 public static class CreditCardExtensions
 {
-    static Validation<Error, int> CharToDigit(char ch) =>
+    public static Validation<Error, uint> CharToDigit(char ch) =>
         ch is >= '0' and <= '9'
-            ? (ch - '0')
-            : Error.New("Invalid character");
-    
-    
+            ? (uint)(ch - '0')
+            : Error.New($"Invalid character: {ch}");
+
+    public static Validation<Error, IEnumerable<uint>> ValidateAllDigits(string value)
+    {
+        return value.ToCharArray()
+            .Map(CharToDigit)
+            .Sequence();
+    }
+
+    public static Validation<Error, uint> ValidateLength(string value, int length) =>
+        value.Length == length ?
+            value: 
+            Error.New($"Length should be {length}");
+
+    public static Validation<Error, uint> ValidateInt(string value) =>
+        ValidateAllDigits(value)
+            .Map(_ => uint.Parse(value));
+
+    public static Validation<Error, Cvv> ValidateCVV(string value)
+    {
+        Func<uint, string, Cvv> createCvv = (n, _) => new Cvv(n);
+        var result = createCvv
+            .Map(ValidateInt(value))
+            .Apply(ValidateLength(value, 3));
+    }
 }
